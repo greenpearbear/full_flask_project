52793d08625adf5f0d58b7929064e22198990cdb
Tw0_notebook <romdmitrbsk1701@gmail.com>
2022-03-10 12:00:01 +0300

    Начало работы над user.

--- views/user_views.py
+++ views/user_views.py
@@ -2,42 +2,33 @@ from flask_restx import Resource, Namespace
 from flask import request
 from dao.model.user_model import UserSchema
 from implemented import user_service
-from helpers.decorators import admin_required
+from helpers.decorators import auth_required
 
 users_ns = Namespace('user')
 
 
 @users_ns.route('/')
 class UsersView(Resource):
-    @admin_required
+    @auth_required
     def get(self):
-        all_users = user_service.get_all()
-        return UserSchema(many=True).dump(all_users), 200
-
-
-@users_ns.route('/<int:uid>')
-class UserView(Resource):
-    @admin_required
-    def get(self, uid: int):
         try:
-            user = user_service.get_one(uid)
+            user = user_service.get_one(1)
             return UserSchema().dump(user), 200
         except Exception as e:
             return str(e), 404
 
-    @admin_required
-    def put(self, uid: int):
+    @auth_required
+    def patch(self):
+        pass
+
+
+@users_ns.route('/password')
+class UserView(Resource):
+    @auth_required
+    def put(self):
         try:
             req_json = request.json
-            user = user_service.put(uid, req_json)
+            user = user_service.put(1, req_json)
             return UserSchema().dump(user), 200
-        except Exception as e:
-            return str(e), 404
-
-    @admin_required
-    def delete(self, uid: int):
-        try:
-            user_service.delete(uid)
-            return "", 204
         except Exception as e:
             return str(e), 404
